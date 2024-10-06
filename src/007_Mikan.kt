d7261d1e57e2c75e41ad83753e1360e4c94b3f97
tapple <hseifhegruh@gmail.com>
2024-10-06 11:09:38 +0900

    10/06

---
+++ src/007_Mikan.kt
@@ -0,0 +1,30 @@
+//https://paiza.jp/works/mondai/c_rank_skillcheck_archive/mikan_00/edit?language_uid=kotlin
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main(args: Array<String>) {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val NMstr = br.readLine()
+    val N = Integer.parseInt(NMstr.split(" ")[0])
+    val M = Integer.parseInt(NMstr.split(" ")[1])
+    var w: Int
+    var work: Int
+    for (i in 1..M){
+        w = Integer.parseInt(br.readLine())
+        if(w <= N){
+            println(N)
+            continue
+        }
+        work = w % N
+        if(work == 0){
+            println((w / N) * N)
+            continue
+        }
+        if(work < (N / 2)){
+            println((w / N) * N)
+        } else {
+            println(((w + N) / N) * N)
+        }
+    }
+}
--- src/Main.kt
+++ src/Main.kt
@@ -2,6 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.Scanner
 fun main() {
+    println(8 / 6)
+    System.exit(0)
     val name = "Kotlin"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
