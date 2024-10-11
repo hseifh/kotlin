48355c00097563d778c536a6b1237f1e54893178
tapple <hseifhegruh@gmail.com>
2024-10-12 03:27:09 +0900

    10/12

--- src/010.kt
+++ src/010.kt
@@ -5,7 +5,7 @@ class Person(var name: String, var age: Int)
 
 fun main(args: Array<String>) {
 
-    val  p = Person("たかはし",34)
+    val  p = Person("鈴木",23)
     println(p.name + " " + p.age)
     p.name = "佐藤"
     p.age = 43
