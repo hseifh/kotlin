01f1ee170016b0c9d47bc48438123ac50715a83a
tapple <hseifhegruh@gmail.com>
2024-10-13 02:55:48 +0900

    10/13

--- src/Main.kt
+++ src/Main.kt
@@ -4,7 +4,7 @@ import java.util.Scanner
 fun main() {
     println(15 / 6)
     System.exit(0)
-    val name = "Kotlin"
+    val name = "Kotlina"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     println("Hello, " + name + "!saa")
