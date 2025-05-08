894708c8160162288c632759ec981aa89a3eae17
tapple <hseifhegruh@gmail.com>
2025-05-08 22:51:35 +0900

    05/08

--- src/014_A020.kt
+++ src/014_A020.kt
@@ -4,12 +4,12 @@ import java.io.InputStreamReader
 fun main(args: Array<String>) {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val N = br.readLine().toInt()
-    var t = Array(N + 2) { Array(N + 2) { "#" } }
+    var t = Array(N + 2) { Array(N + 2) { '#' } }
     var stStr:String
     for (i in 1 until (t.size - 1)) {
         stStr = br.readLine()
         for (j in 1 until (t[i].size - 1)) {
-            t[i][j] = stStr.get(j - 1).toString()
+            t[i][j] = stStr[j - 1]
         }
     }
     val M = br.readLine().toInt()
@@ -17,10 +17,10 @@ fun main(args: Array<String>) {
     for (i in 0 until M) {
         p[i] = br.readLine()
     }
-    var work : String
+    var work : Char
     var flag : Boolean
     a@ for (i in 0 until M) {
-        work = p[i].get(0).toString()
+        work = p[i][0]
         flag = false
         b@ for (j in 1 until (t.size - 1)) {
             c@ for (k in 1 until (t[j].size - 1)) {
@@ -35,34 +35,34 @@ fun main(args: Array<String>) {
         println(if (flag) "yes" else "no")
     }
 }
-fun check(t: Array<Array<String>>, row: Int, col: Int, p: String, index: Int): Boolean {
+fun check(t: Array<Array<Char>>, row: Int, col: Int, p: String, index: Int): Boolean {
     if(p.length == index){
         return true
     }
     //配列クローン
     val tCopy = Array(t.size) { i -> t[i].clone() }
-    tCopy[row][col] = "#"
+    tCopy[row][col] = '#'
 
     //上が空白
-    if(p.get(index).toString() == t[row - 1][col]){
+    if(p[index] == t[row - 1][col]){
         if(check(tCopy,row - 1,col,p,index + 1)){
             return true
         }
     }
     //右が空白
-    if(p.get(index).toString() == t[row][col + 1]){
+    if(p[index] == t[row][col + 1]){
         if(check(tCopy,row,col + 1,p,index + 1)){
             return true
         }
     }
     //下が空白
-    if(p.get(index).toString() == t[row + 1][col]){
+    if(p[index] == t[row + 1][col]){
         if(check(tCopy,row + 1,col,p,index + 1)){
             return true
         }
     }
     //左が空白
-    if(p.get(index).toString() == t[row][col - 1]){
+    if(p[index] == t[row][col - 1]){
         if(check(tCopy,row,col - 1,p,index + 1)){
             return true
         }
