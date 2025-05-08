11ca96e50d0163c867f3cdfe4840cc36eb0610d4
tapple <hseifhegruh@gmail.com>
2025-05-09 03:04:04 +0900

    05/09

--- src/014_A020.kt
+++ src/014_A020.kt
@@ -1,31 +1,26 @@
-import java.io.BufferedReader
-import java.io.InputStreamReader
-
 fun main(args: Array<String>) {
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    val N = br.readLine().toInt()
+    val N = readln().toInt()
     var t = Array(N + 2) { Array(N + 2) { '#' } }
     var stStr:String
     for (i in 1 until (t.size - 1)) {
-        stStr = br.readLine()
+        stStr = readln()
         for (j in 1 until (t[i].size - 1)) {
             t[i][j] = stStr[j - 1]
         }
     }
-    val M = br.readLine().toInt()
-    var p = Array<String>(M){ "" }
-    for (i in 0 until M) {
-        p[i] = br.readLine()
+    val M = readln().toInt()
+    val pAry = Array(M) {
+        readln()
     }
     var work : Char
     var flag : Boolean
-    a@ for (i in 0 until M) {
-        work = p[i][0]
+    a@ for (p in pAry) {
+        work = p[0]
         flag = false
         b@ for (j in 1 until (t.size - 1)) {
             c@ for (k in 1 until (t[j].size - 1)) {
                 if(t[j][k] == work){
-                    flag = check(t,j,k,p[i],1)
+                    flag = check(t,j,k,p,1)
                     if(flag){
                         break@b
                     }
@@ -35,7 +30,14 @@ fun main(args: Array<String>) {
         println(if (flag) "yes" else "no")
     }
 }
-fun check(t: Array<Array<Char>>, row: Int, col: Int, p: String, index: Int): Boolean {
+fun check(
+    t: Array<Array<Char>>,
+    row: Int,
+    col: Int,
+    p: String,
+    index: Int):
+        Boolean {
+
     if(p.length == index){
         return true
     }
