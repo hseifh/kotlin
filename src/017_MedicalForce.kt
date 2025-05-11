bf31fe16ad6d36e9eadb4836510309a7a17f24d2
tapple <hseifhegruh@gmail.com>
2025-05-11 21:02:08 +0900

    05/11

--- src/017_MedicalForce.kt
+++ src/017_MedicalForce.kt
@@ -1,54 +1,35 @@
 fun main(args: Array<String>) {
-    val N = readln().toInt()
-    val dStr = readln().filterNot { it.isWhitespace() }
-    val flagAry = Array<Boolean>(N) { false }
-    val seven = 7
-    var i = 0
-    while (i < (dStr.length - (seven - 1))) {
-    //while (i < dStr.length) {
-        //println(dStr.substring(i,(seven + i)))
-        //println(i)
-
-        if((dStr.substring(i,(seven + i)).count { it == '0' }) >= 2 ){
-            //println(i)
-            for (j in i until (i + seven)) {
-                //println(i)
-                flagAry[j] = true
-            }
-            i += seven
-        }else{
-            i++
-        }
+    val original = intArrayOf(1, 2, 2, 3, 4, 4, 5)
+    val unique = original.distinct()
+    for(o in original){
+        println(o)
     }
-    println(i)
-    while (i < dStr.length) {
-        //println(i - (seven - 1) + i)
-        if(i >= seven) {
-            println(dStr.substring(i - (seven - 1), i))
-            if ((dStr.substring(i - (seven - 1), i).count { it == '0' }) >= 2) {
-                flagAry[i] = true
-            }
-            i++
-        }
-    }
-    var ans = 0
-    var serial = 0
-    /*
-    for (flag in flagAry) {
-        println(flag)
-    }
-
-     */
-
-    for (flag in flagAry) {
-        if(flag){
-            serial++
-            if(serial > ans){
-                ans = serial
-            }
-        }else{
-            serial = 0
-        }
-    }
-    println(ans)
 }
+
+fun solution(arr: Array<Int>): Array<Int> {
+    var ary = Array(arr.size) {0}
+    for(i in 0 until arr.size){
+        ary[i] = arr[i] * 2
+    }
+    return ary
+}
+
+fun solution(nums: Array<Int>, target: Int): Int {
+    var ans = 0
+    a@ for(i in 0 until nums.size){
+        b@ for(j in i until nums.size){
+            if((nums[i] + nums[j]) == target){
+                println("" + i + "と" + j)
+                ans++
+            }
+        }
+    }
+    return ans
+}
+
+
+fun solution(reservations: Array<Array<Int>>): Int {
+
+    return 1
+}
+
