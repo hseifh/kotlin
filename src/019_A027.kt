f39101877e56bb752d44e29088d1f4939b80d4a3
tapple <hseifhegruh@gmail.com>
2025-05-31 20:27:08 +0900

    05/31

--- src/019_A027.kt
+++ src/019_A027.kt
@@ -3,48 +3,50 @@ fun main(args: Array<String>) {
     val H = brStr.split(" ")[0].toInt()
     val W = brStr.split(" ")[1].toInt()
     var bArray = Array(H + 2) {
-        Array(W + 2) { 0 }
+        Array(W + 2) { '.' }
     }
     for (i in 1 until (bArray.size - 1)) {
         brStr = readln()
         for (j in 1 until (bArray[i].size - 1)) {
             if (brStr[j - 1] == '#') {
-                bArray[i][j] = 1
+                bArray[i][j] = brStr[j - 1]
             }
         }
     }
-    /*
-    println("" + H + " " + W)
-    for (i in 0 until bArray.size) {
-        for (j in 0 until bArray[i].size) {
-            print(bArray[i][j])
-        }
-        println()
-    }
-     */
-    var count: Int = 2
-    var continueFlag: Boolean = true
+    var cList = mutableListOf<Coordinate3>()
     var ans = 0
-    while(continueFlag) {
-        println(count)
-        continueFlag = false
+    fun isSea(row:Int,col:Int): Boolean {
+        if(bArray[row][col] == '.'){
+            return true
+        }
+        return false
+    }
+    while(true) {
         for (i in 1 until (bArray.size - 1)) {
             for (j in 1 until (bArray[i].size - 1)) {
-                if(
-                    (bArray[i][j] == 0 ||
-                    bArray[i][j] != count) &&
-                    bArray[i][j] != 1
-                    
-                    ){
-                    bArray[i][j] = count
-                    continueFlag = true
+                if(bArray[i][j] == '#'){
+                    if(isSea(i - 1,j)){
+                        cList.add(Coordinate3(i,j))
+                    }else if(isSea(i,j + 1)){
+                        cList.add(Coordinate3(i,j))
+                    }else if(isSea(i + 1,j)){
+                        cList.add(Coordinate3(i,j))
+                    }else if(isSea(i,j - 1)){
+                        cList.add(Coordinate3(i,j))
+                    }
                 }
             }
         }
-        if(continueFlag){
+        if(cList.size == 0){
+            break
+        }else{
             ans++
         }
-        count++
+        for (c in cList){
+            bArray[c.i][c.j] = '.'
+        }
+        cList.clear()
     }
     println(ans)
 }
+class Coordinate3(var i: Int, var j: Int)
