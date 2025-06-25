cbb3368bfd92e4ea878aa99e3d2c6b7b7e1c4cbe
tapple <hseifhegruh@gmail.com>
2025-06-26 01:04:04 +0900

    06/25

---
+++ src/Main03.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main03 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("HW");
+
+    }
+}
